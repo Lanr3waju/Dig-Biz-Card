8539129dfccea07cb50b61725534186e617a71cc
Abass <Abassolanrewaju111@gmail.com>
2022-09-18 14:35:07 +0100

    Add content to Info-section

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 export default function About() {
     return (
         <>
-            
+            <h1>I am Abass Olanrewaju Abdul Wasi</h1>
         </>
     )
 }
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,7 +1,14 @@
 export default function Info() {
     return (
-        <>
-            
-        </>
+        <section className="info">
+        <img alt="Abass" className="info-Image" />
+        <h3>Abass Olanrewaju</h3>
+        <p className="info-title">Frontend Developer</p>
+        <p><small>abassolanrewaju.website</small></p>
+        <div>
+        <button className="info-email-btn"><img className="info-email-icon" alt="Email-Icon" />Email</button>
+        <button className="info-linkedin-btn"><img className="info-linkedin-icon" alt="LinkedIn-Icon" />LinkedIn</button>
+        </div>
+        </section>
     )
 }
