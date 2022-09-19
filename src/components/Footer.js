35115b33b80b0501112a58dd41887b0df38841e2
Abass <Abassolanrewaju111@gmail.com>
2022-09-19 04:03:44 +0100

    Add content to footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,10 @@
 export default function Footer() {
     return (
-        <>
-            
-        </>
+        <footer className="footer">
+            <a href="https://www.linkedin.com/in/abass-abdul-wasi-53883422a/"><img alt="twitter-icon"/></a>
+            <a href="https://www.linkedin.com/in/abass-abdul-wasi-53883422a/"><img alt="fb-icon"/></a>
+            <a href="https://www.linkedin.com/in/abass-abdul-wasi-53883422a/"><img alt="instagram-icon"/></a>
+            <a href="https://www.linkedin.com/in/abass-abdul-wasi-53883422a/"><img alt="github-icon"/></a>
+        </footer>
     )
 }
